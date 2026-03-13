52793341db526a0943011b6cc675af71c40afab8
Brahman WebTech <109435061+bmwtch@users.noreply.github.com>
2026-03-13 18:58:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-# main.py
 import tkinter as tk
 from gui.app_gui import ControlPanelApp
 import json
 import os
+from modules import php_manager
 
 # Config in AppData\Roaming\HostLoca
 CONFIG_PATH = os.path.join(os.getenv("APPDATA"), "HostLoca", "settings.json")
 
 def load_settings():
     defaults = {
-        "xampp_path": "",
+        "xampp_path": "C:/xampp",
         "htdocs_path": "",
         "sql_folder": "",
         "db_user": "root",
@@ -34,9 +34,14 @@ def save_settings(settings):
 def main():
     settings = load_settings()
 
+    # Run fallback check on startup
+    fallback_msg = php_manager.check_fallback(settings)
+    if fallback_msg:
+        print(fallback_msg)
+
     root = tk.Tk()
     root.title("HostLoca - XAMPP Controller || © Brahman WebTech - www.bmwtech.in")
-    root.geometry("800x550")
+    root.geometry("800x650")
     root.resizable(False, False)
     root.option_add("*Font", ("Segoe UI", 10))
     ControlPanelApp(root, settings, save_settings)
